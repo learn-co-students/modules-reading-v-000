78829930538d6fb4b4e2d3034ae1c53d50d31da2
lizdenhup <lizdenhup@gmail.com>
2015-12-24 00:39:34 -0500

    finished modules reading lab

--- lib/ballerina.rb
+++ lib/ballerina.rb
@@ -1,2 +1,16 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Ballerina
+  include Dance
+  extend MetaDancing
+  #the Dance module's methods are available to the
+  #ballerina and kid classes as instance methods
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+
+end 
