f9db5694fc3d7ce6f1bdb5c3bb9e5d1802d3d9c4
gnfisher <me@gnfisher.com>
2016-03-12 11:17:31 -0300

    Done.

--- lib/ballerina.rb
+++ lib/ballerina.rb
@@ -1,2 +1,17 @@
-require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 require_relative './dance_module.rb'
+require_relative './class_methods_module.rb'
+
+class Ballerina
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  include Dance
+  extend MetaDancing
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
