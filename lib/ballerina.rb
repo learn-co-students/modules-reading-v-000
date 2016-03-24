e38e75024ddbb3de99d3fb66ec74ad63dc935ef4
McN33ly <rmcneely23@gmail.com>
2016-03-24 12:44:40 -0400

    Done.

--- lib/ballerina.rb
+++ lib/ballerina.rb
@@ -1,2 +1,17 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Ballerina
+
+extend FancyDance::ClassMethods, MetaDancing
+include FancyDance::InstanceMethods, Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+
+end
