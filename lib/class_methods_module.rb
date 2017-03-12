61176f8301062ce24ac029ab0e71188b01677219
charlie lee <ctlee9096@gmail.com>
2017-03-12 15:38:21 +0000

    Done.

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -2,10 +2,10 @@ module MetaDancing
   def metadata
     "This class produces objects that love to dance."
   end
-  #class Dancer
-  #  extend MetaDancing
-  #end
-  #class Kid
-  #  extend MetaDancing
-  #end
+  class Dancer
+    extend MetaDancing
+  end
+  class Kid
+    extend MetaDancing
+  end
 end
