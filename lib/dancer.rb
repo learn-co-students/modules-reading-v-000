4a6909d28766acf851a61d583dd17761c6206cdd
Leonardo Guzman <lguzman@me.com>
2017-04-28 15:18:34 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,18 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+  #extend is used to add additional functionality to our classes via class methods.
+  extend FancyDance::ClassMethods
+  #include is used to add functionality to our classes via instance methods.
+  include FancyDance::InstanceMethods
+
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
