79b88a7a3c6bfc2068e479a553d927ef72181bac
Aly Alidina <aly_alidina@hotmail.com>
2017-06-05 11:34:40 +0000

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,6 @@
+module MetaDancing
+
+  def metadata #method intended to be used as class methods in the Kid and Dancer classes
+    "This class produces objects that love to dance."
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+class Kid
+  extend FancyDance::ClassMethods
+  #`include` is used to add functionality to our classes via instance methods.
+  include FancyDance::InstanceMethods #lend a module's methods to a class as class methods
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
