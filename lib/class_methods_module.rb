710b398fdcd52552144cf1cc39e6f1675da34a02
Alex <github email address>
2017-06-11 21:09:09 -0400

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,7 @@
+#define the module which wraps methods that can be reused
+
+module MetaDancing
+  def metadata
+ "This class produces objects that love to dance."
+  end
+end
--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,17 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  #a module groups multiple instance methods to be used in multiple classes
+  #To lend our two classes all of the methods of the Dance module, we use the include keyword:
+  #If we use include keyword, we allow our classes to use all of the methods of the included module as instance methods.
+  include Dance
+#extend allows you to copy over class methods
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
