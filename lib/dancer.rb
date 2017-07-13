60f9f50f0f79a6c40dc28d933dac9ba53888513c
NickisKnowledge <lifechangingstuff@gmail.com>
2017-07-12 23:06:29 -0500

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,16 @@
+# must have these so program can find and refer to files
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+# file defines one module and specifies which methods
+require_relative './fancy_dance.rb'
+
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
+	attr_accessor :name
+	
+	def initialize(name)
+		@name = name
+	end
+end
