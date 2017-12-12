93b3cb88cec0025fc8e267359387d2a65aa65e82
Yina Zhu <yinagit@gmail.com>
2017-12-12 06:46:42 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,27 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+# class Dancer
+#   include Dance
+#
+#   attr_acccessor :name
+#
+#   def initalize(name)
+#     @name = name
+#   end
+#
+# end
+
+class Dancer
+  include Dance
+extend MetaDancing
+  # extend FancyDance::ClassMethods
+  # include FanceDance::InstanceMethods
+
+
+attr_accessor :name
+
+def initialize(name)
+  @name = name
+end
+end
