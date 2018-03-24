5a5f9affb3c3a6aff612b52fe0e4811b33d6dd02
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-24 03:50:10 +0000

    Done.

---
+++ lib/dance_module.rb
@@ -0,0 +1,18 @@
+module Dance
+  def twirl
+    "I'm twirling!"
+  end # twirl
+
+  def jump
+    "Look how high I'm jumping!"
+  end # jump
+
+  def pirouette
+    "I'm doing a pirouette"
+  end # pirouette
+
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all."
+  end # take_a_bow
+
+end # Dance module
---
+++ lib/fancy_dance.rb
@@ -0,0 +1,38 @@
+module FancyDance
+
+############################################
+# INSTANCE METHODS
+############################################
+  module InstanceMethods
+
+    def twirl
+      "I'm twirling!"
+    end # twirl
+
+    def jump
+      "Look how high I'm jumping!"
+    end # jump
+
+    def pirouette
+      "I'm doing a pirouette"
+    end # pirouette
+
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end # take_a_bow
+
+  end # InstanceMethods module
+
+############################################
+# CLASS METHODS
+############################################
+
+  module ClassMethods
+
+    def metadata
+      "This class produces objects that love to dance."
+    end # metadata
+
+  end # ClassMethods module
+
+end # FancyDance module
