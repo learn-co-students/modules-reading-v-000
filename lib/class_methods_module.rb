abb20817d84cc6e1686c287e14fb8cd25655b229
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-06 22:09:55 +0000

    Done.

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,7 +1,6 @@
-module ClassMethods
+module MetaDancing
 
-    def metadata
-      "This class produces objects that love to dance."
-    end
+  def metadata
+    "This class produces objects that love to dance."
   end
 end
