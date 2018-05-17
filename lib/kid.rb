5ad9a0db05cf5347f19bb1c570872f557582852c
David Moore <hey@iamdavidmoore.com>
2018-05-17 21:08:24 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  # When we use the include keyword, we all our classes to use all of the methods of the included module as instance moethds.
+  # However, to lend module's methods to a class as class methods, we use the extend keyword.
+  # We refer to the name-spaced modules or classes with ::. This syntax references the parent and child relationship of the nested modules.
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
