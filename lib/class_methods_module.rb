ad66e34cae1a119fb936dad98e4f716a92e3b0e8
Kate Preston <katejsenger@yahoo.com>
2018-11-21 22:02:05 +0000

    Done.

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -3,5 +3,5 @@ module ClassMethods
     def metadata
       "This class produces objects that love to dance."
     end
-  end
-end
+ end
+
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,9 @@
 require_relative './fancy_dance.rb'
+require_relative './dance_module.rb'
+require_relative './class_methods_module.rb'
 class Kid
-   
+  include Dance
+   extend MetaDancing
    extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
  
