90f736cc03c0a857539183382e52386ef4c48717
Gigi M <mmgamao1@gmail.com>
2019-04-01 21:58:47 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,12 @@ require_relative './class_methods_module.rb'
 
 
 class Kid
+  
   include Dance
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   extend MetaDancing
+  
   attr_accessor :name 
   
   def initialize(name)
