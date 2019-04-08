d241a1a6604939810e7666e30b0415271fe0175d
Toni-Ty <antonique2479@yahoo.com>
2019-04-08 06:29:06 +0000

    Done.

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,2 +1,7 @@
 # Modules allow us to collect and bundle a group of methods
 # and make those methods available to any number of classes.
+module MetaDancing
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,8 +1,10 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 class Kid
-  include Dance
+  extend FancyDance::ClassMethods #:: This syntax references the parent and child relationship of the nested modules.
+  include FancyDance::InstanceMethods
 
 attr_accessor :name
 
